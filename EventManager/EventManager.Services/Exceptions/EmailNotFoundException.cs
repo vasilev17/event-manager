029b6348f1ada56137d72be4b0a53d29ce8bb5e6
Alex Kadiyski <alex_kadiyski2003@abv.bg>
2024-12-04 00:43:17 +0200

    UserService unit tests- 2 possible bugs

---
+++ EventManager/EventManager.Services/Exceptions/EmailNotFoundException.cs
@@ -0,0 +1,8 @@
+﻿public class EmailNotFoundException : Exception
+{
+    public EmailNotFoundException() { }
+
+    public EmailNotFoundException(string message) : base(message) { }
+
+    public EmailNotFoundException(string message, Exception innerException) : base(message, innerException) { }
+}
