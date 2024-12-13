bce8b25912233f41f42c6bed528586b3723d000a
marto55 <martin_my@abv.bg>
2024-12-13 11:26:25 +0200

    adding our colors to the tailwind.config

--- FrontEndApp/tailwind.config.js
+++ FrontEndApp/tailwind.config.js
@@ -3,7 +3,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary : "#40ddc7",
+        primary2 : "#5abab7",
+        secondary : "#29005d",
+        secondary2 : "#cec2dd",
+      }
+
+    },
   },
   plugins: [],
 };
