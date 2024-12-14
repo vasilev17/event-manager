70d51d8a453f6f49d65314dbbe3e5fc63cf6fe3b
marto55 <martin_my@abv.bg>
2024-12-14 18:58:10 +0200

    Adding Card component

---
+++ FrontEndApp/src/components/Card.jsx
@@ -0,0 +1,27 @@
+export default function Card({ picture, title, description }) {
+  const trimmedTitle =
+    title.length > 25 ? title.substring(0, 30) + "..." : title;
+
+  const trimmedDescription =
+    description.length > 50
+      ? description.substring(0, 50) + "..."
+      : description;
+
+  return (
+    <div className="relative max-w-96">
+      <div className="rounded-t-3xl bg-primary2 shadow-md overflow-hidden">
+        <img
+          src={picture}
+          alt="stew"
+          className="h-32 sm:h-48 w-full object-cover"
+        ></img>
+      </div>
+      <div className="p-4 pb-5 -mt-6 rounded-3xl bg-primary absolute w-full">
+        <span className="font-bold text-xl text-white">{trimmedTitle}</span>
+        <span className="block text-xl font-semibold text-white">
+          {trimmedDescription}
+        </span>
+      </div>
+    </div>
+  );
+}
