f405ebd6c89b4817b32784911b8847b13ccbff99
marto55 <martin_my@abv.bg>
2024-12-14 22:10:53 +0200

    Adding Component for searching events by place and date PlaceAndDateSearch.jsx

--- FrontEndApp/src/pages/HomePage.jsx
+++ FrontEndApp/src/pages/HomePage.jsx
@@ -1,32 +1,37 @@
 import Footer from "../components/Footer";
 import MailAbonament from "../components/MailAbonament";
 import CardsRow from "../components/CardsRow";
-import stew from "../assets/stew.jpg";
+import pictureFromAssetsFolder from "../assets/pictureFromAssetsFolder.jpg";
+import PlaceAndDateSearch from "../components/PlaceAndDateSearch";
 
 export default function HomePage() {
+
+  const hardCodedPictureLink = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.rbVMAX7i5iH4MrkykUiy9gHaEK%26pid%3DApi&f=1&ipt=58261e1cbfc98b5409839b963a42f3f62be6efc4851a24b84d4ab3730c5f3a97&ipo=images";
+
   return (
     <div>
+      <PlaceAndDateSearch />
       <CardsRow
         mainTitle="Концерти"
-        card1Picture="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.rbVMAX7i5iH4MrkykUiy9gHaEK%26pid%3DApi&f=1&ipt=58261e1cbfc98b5409839b963a42f3f62be6efc4851a24b84d4ab3730c5f3a97&ipo=images"
+        card1Picture={hardCodedPictureLink}
         card1Title="Заглавие 1"
         card1Description="Кратко описание 2 което не e толкова кратко свъшност, за това ще се отреже края му"
-        card2Picture="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.rbVMAX7i5iH4MrkykUiy9gHaEK%26pid%3DApi&f=1&ipt=58261e1cbfc98b5409839b963a42f3f62be6efc4851a24b84d4ab3730c5f3a97&ipo=images"
+        card2Picture={hardCodedPictureLink}
         card2Title="Заглавие 2"
         card2Description="Кратко описание 2"
-        card3Picture={stew}
+        card3Picture={pictureFromAssetsFolder}
         card3Title="Заглавие 3"
         card3Description="Кратко описание 3"
       />
       <CardsRow
         mainTitle="Туризъм"
-        card1Picture="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.rbVMAX7i5iH4MrkykUiy9gHaEK%26pid%3DApi&f=1&ipt=58261e1cbfc98b5409839b963a42f3f62be6efc4851a24b84d4ab3730c5f3a97&ipo=images"
+        card1Picture={hardCodedPictureLink}
         card1Title="Заглавие 1"
         card1Description="Кратко описание 2 което не e толкова кратко свъшност, за това ще се отреже края му"
-        card2Picture="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.rbVMAX7i5iH4MrkykUiy9gHaEK%26pid%3DApi&f=1&ipt=58261e1cbfc98b5409839b963a42f3f62be6efc4851a24b84d4ab3730c5f3a97&ipo=images"
+        card2Picture={hardCodedPictureLink}
         card2Title="Заглавие 2"
         card2Description="Кратко описание 2"
-        card3Picture={stew}
+        card3Picture={pictureFromAssetsFolder}
         card3Title="Заглавие 3"
         card3Description="Кратко описание 3"
       />
