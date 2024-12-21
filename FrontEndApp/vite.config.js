14d236f8d75f68d38b69f0eff6301e4a04e51229
marto55 <martin_my@abv.bg>
2024-12-21 18:00:09 +0200

    Adding proxy server for development to avoid SOP

--- FrontEndApp/vite.config.js
+++ FrontEndApp/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': { // The path you want to proxy
+        target: 'https://localhost:7223', // Your backend URL
+        changeOrigin: true, // Required for CORS issues
+        secure: false, // Set to true if your backend uses HTTPS with a valid certificate
+        rewrite: (path) => path.replace(/^\/api/, ''), // Optional: remove the /api prefix
+      },
+      // You can add more proxy configurations here for other paths
+    },
+  }
 })
